1d095983b3e034f7c9fb8c36b8492a8039ebe0b2
Its-Isaku <Isai.almeraz@uabc.edu.mx>
2026-03-04 18:14:01 -0800

    Initial Commit

---
+++ IsTilted/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IsTilted
+//
+//  Created by Isai Magdaleno Almeraz Landeros on 04/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IsTilted/IsTiltedApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsTiltedApp.swift
+//  IsTilted
+//
+//  Created by Isai Magdaleno Almeraz Landeros on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct IsTiltedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
